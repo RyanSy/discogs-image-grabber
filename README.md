ad11e1fead15fd37d79847f0b96ee8256cd10faa
Ryan Sy <ryanbsy@gmail.com>
2018-11-16 01:39:02 +0000

    now using sync-request to make synchronous requests

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-Discogs Image Grabber is a tool that searches the Discogs database for a specific release and downloads the main image for that release. To make things easier and faster, the first image url from the first result is downloaded.
+Discogs Image Grabber is a tool that searches the Discogs database for a specific release and downloads the main image for that release. To make things easier and faster, the first image url from the first result is downloaded. Genres and image file names are also downloaded to txt files for copying and pasting into a csv file. Requests to the Discogs API are made synchronously so they remain in order.
 
-Discogs API Grabber is basically the same tool, using promises instead of callbacks.
+Index.js is a simple example of using the Google Sheets API.
+
+
--- discogs-image-grabber.js
+++ discogs-image-grabber.js
@@ -1,44 +1,109 @@
 require('dotenv').config();
-const request = require('request');
+const request = require('sync-request');
+const fs = require('fs');
 const download = require('image-downloader');
+const inventory = require('./inventory.js');
 
-/*
-to do:
-set timeout to perform 60 requests per minute as per discogs rate limit
-use array of product titles for search queries
-*/
-
-var searchQuery = 'Nas - Illmatic';
-var searchQueryModified = searchQuery.replace(/\s/g, '+');
-var imageUrl;
-
-// get image url
-var options = {
-  url: 'https://api.discogs.com/database/search?q=' + searchQueryModified + '&per_page=1&token=' + process.env.ACCESS_TOKEN,
-  headers: {
-    'User-Agent': 'request'
-  }
-};
-request(options, function(error, response, body) {
-  console.log('error: ', error);
-  console.log('status code: ', response.statusCode);
-  var jsonBody = JSON.parse(body);
-  console.log('body:\n ', jsonBody.results[0].cover_image);
-  imageUrl = jsonBody.results[0].cover_image;
-
-  // download image
-  var options2 = {
-    url: imageUrl,
-    dest: '../images/' + searchQuery + '.jpg',
-    headers: {
-      'User-Agent': 'request'
-    }
-  };
-  download.image(options2)
-    .then(({filename, image}) => {
-      console.log('file saved to ', filename);
-    })
-    .catch((err) => {
-      console.error(err);
+function getInfo(release) {
+    console.log('getInfo(' + release + ') called');
+    // get info from discogs
+    let releaseModified = encodeURI(release.replace(/\s/g, '+'));
+    let releaseNoSpaces = (release.replace(/\s/g, '')).toLowerCase();
+    let res = request('GET', 'https://api.discogs.com/database/search', {
+        headers: {
+        'user-agent': 'discogs-info-grabber'
+        },
+        qs: {
+        'q': releaseModified,
+        'per_page': 1,
+        'format': 'Vinyl',
+        'token': process.env.ACCESS_TOKEN
+        }
     });
-});
+    let json = JSON.stringify(res.body);
+    let bufferOriginal = Buffer.from(JSON.parse(json).data);
+    let jsonResponse = JSON.parse(bufferOriginal.toString('utf8'));
+    if (jsonResponse.results[0] === undefined) {
+        console.log('info undefined\n');
+        // write errors to _errors.txt
+        fs.appendFileSync('_errors.txt', release + '\n', (err) => {
+            if (err) throw err;
+        });
+        // write blank space to _image-files.txt
+        fs.appendFileSync('_image-files.txt', '\n', (err) => {
+        if (err) throw err;
+        });
+        // write blank space to _categories.txt
+        fs.appendFileSync('_categories.txt', '\n', (err) => {
+        if (err) throw err;
+    });
+        return;
+    }
+    let title = jsonResponse.results[0].title;
+    let genre = (jsonResponse.results[0].genre)[0];
+    let style = jsonResponse.results[0].style;
+    let image = jsonResponse.results[0].cover_image;
+    let id = jsonResponse.results[0].id;
+
+    // download image
+    let options = {
+            url: image,
+            headers: {
+                'User-Agent': 'request'
+            },
+            dest: 'data/images/' + releaseNoSpaces + '.jpg'                  
+        }
+    async function downloadIMG() {
+        try {
+            const { filename, image } = await download.image(options);
+            
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    downloadIMG()
+    
+    // write image names to _image-files.txt
+    fs.appendFileSync('_image-files.txt', (releaseNoSpaces + '.jpg\n'), (err) => {
+        if (err) throw err;
+    });
+            
+    // write genres to _genres.txt
+    fs.appendFileSync('_categories.txt', ('Vinyl/' + genre + '\n'), (err) => {
+        if (err) throw err;
+    });
+    
+    // write tracklist to file
+    function getMasterInfo(id) {
+        console.log('getMasterInfo(' + id + ') called');
+        let res2 = request('GET', 'https://api.discogs.com/masters/' + id, {
+            headers: {
+            'user-agent': 'discogs-info-grabber'
+            },
+            qs: {
+            'token': process.env.ACCESS_TOKEN
+            }
+        });
+        let json2 = JSON.stringify(res2.body);
+        let bufferOriginal2 = Buffer.from(JSON.parse(json2).data);
+        let jsonResponse2 = JSON.parse(bufferOriginal2.toString('utf8'));
+        if (jsonResponse2 === undefined) {
+            console.log('error getting master info\n');
+            return;
+        }
+        let tracklist = jsonResponse2.tracklist;
+        // for (let track of tracklist) {
+        //     fs.appendFileSync(title + '.txt', '<li>' + track.title + '</li>', (err) => {
+        //       if (err) throw err;
+        //       return;
+        //     });
+        // }
+    }
+    getMasterInfo(id);
+    
+    console.log('end\n')
+} // end getInfo() function
+
+for (let product of inventory) {
+    getInfo(product);
+}
